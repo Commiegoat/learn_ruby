f4a7e08ab21ab61035a1aadf6311ae321d8e33a0
commiegoat <iolson91@gmail.com>
2017-06-15 22:00:44 -0500

    completed 04 pig latin, not without some frustration

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,39 @@
 #write your code here
+
+def add(x,y)
+	puts = x + y
+end
+
+def subtract(x,y)
+	puts = x - y
+end
+
+def sum(array)
+	total = 0
+	array.each do |i|
+		total += i
+	end
+	puts = total
+end
+
+def multiply(array)
+	result = 1
+	array.each do |i|
+		result = i * result
+	end
+	puts = result
+end
+
+def power(x,y)
+	puts = x ** y
+end
+
+def factorial(x)
+	result = 1
+	if x > 0
+		(1..x).each do |i|
+			result *= i
+		end
+	end
+	puts = result 
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,28 @@
 #write your code here
+
+def translate(input)
+	vowels = ["a","e","i","o","u"]
+	word_array = input.split(" ")
+	word_array.each do |word|
+		if vowels.include?(word[0])
+			 word << "ay"
+		else
+			i = 1
+			while i < word.length
+				if (word[i] == "u") && (word[i-1] == "q")
+					word << word[0..i] + "ay"
+					word[0..i] = ""
+					break
+				elsif vowels.include?(word[i])
+					word << word[0..i-1] + "ay"
+					word[0..i-1] = ""
+					break
+				els
+				else
+					i+=1
+				end
+			end
+		end
+	end
+	puts = word_array.join(" ")
+end
