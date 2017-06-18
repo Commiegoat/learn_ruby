ca4ee0902d0ecba2c21c92faa98516f639546375
commiegoat <iolson91@gmail.com>
2017-06-18 18:20:26 -0500

    finished timer exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+	attr_reader :title
+
+    def initialize(title=nil)
+      	@title = title
+    end
+
+    def title=(new_title)
+    	no_caps = ["the", "and", "then", "or", "a", "the", "of", "in", "an"]
+    	array = new_title.split.each do |w|
+    		unless no_caps.include?(w)
+    			w.capitalize!
+    		end
+       	end
+    	array[0].capitalize!
+    	new_title = array.join(' ')
+
+    	@title = new_title
+    end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,48 @@
 class Timer
-  #write your code here
+	attr_reader :seconds
+
+	def initialize(seconds=0)
+		@seconds = seconds
+	end
+
+	def seconds=(seconds)
+		@seconds = seconds
+	end
+
+	def time_string
+		hr = 0 
+		min = 0
+		sec = @seconds
+		time = @seconds
+		while time > 0
+			if sec >= 60
+				min += 1
+				time -= 60
+				sec = time
+				if min >= 60
+					hr += 1
+					min = 0
+				end
+			else
+				sec = time
+				time = 0
+			end
+		end
+
+		def stringer(number)
+			if number < 10
+				number = "0" + number.to_s
+			else
+				number.to_s
+			end
+		end
+
+		hr = stringer(hr)
+		min = stringer(min)
+		sec = stringer(sec)
+		
+		timer_display = "#{hr}:#{min}:#{sec}"
+		puts = timer_display
+	end
+
 end
